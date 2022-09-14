daaa7bd22f5cd1bc14ca8412437302b05c766fd0
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2022-09-14 17:29:45 -0300

    Update ex-1035.java

--- ex-1035.java
+++ ex-1035.java
@@ -1,23 +1,16 @@
-import java.io.IOException;
 import java.util.Scanner;
 
-public class exA {
+       public class Main {
 
-
-    public class Main {
-
-        public static void main(String[] args) throws IOException {
-
-            int A,B,C,D;
-
+        public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
 
-            A = input.nextInt();
-            B = input.nextInt();
-            C = input.nextInt();
-            D = input.nextInt();
+            int A = input.nextInt();
+            int B = input.nextInt();
+            int C = input.nextInt();
+            int D = input.nextInt();
 
-            if ( B > C && D > A && D > A+B && C > 0 && D>0 && A % 2 == 0 ){
+            if ( B > C && D > A && C + D > A && C > 0 && D>0 && A % 2 == 0 ){
 
                 System.out.print("Valores aceitos");
             }
@@ -27,4 +20,4 @@ public class exA {
             }
         }
     }
-}
+
