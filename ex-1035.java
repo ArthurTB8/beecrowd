3134551a89db6eefa90c6007a072e881f5c914ad
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2022-09-16 11:59:26 -0300

    Update ex-1035.java

--- ex-1035.java
+++ ex-1035.java
@@ -1,23 +1,24 @@
+import java.io.IOException;
 import java.util.Scanner;
 
-       public class Main {
+public class Main {
 
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-            int A = input.nextInt();
-            int B = input.nextInt();
-            int C = input.nextInt();
-            int D = input.nextInt();
+      Scanner teclado = new Scanner(System.in);
+      int A,B,C,D;
 
-            if ( B > C && D > A && C + D > A && C > 0 && D>0 && A % 2 == 0 ){
+      A = teclado.nextInt();
+      B = teclado.nextInt();
+      C = teclado.nextInt();
+      D = teclado.nextInt();
 
-                System.out.print("Valores aceitos");
-            }
-            else {
-
-                System.out.print("Valores nao aceitos");
-            }
-        }
+      if ((B > C) && (D > A) && (C+D > A+B) && (C > 0) && (D > 0) && (A%2==0)) {
+        System.out.println("Valores aceitos");
+      }
+      else {
+        System.out.println("Valores nao aceitos");
+      }
     }
 
+}
