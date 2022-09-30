e5fc852e97c41361eed25dcacd9beed2fed1921f
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2022-09-30 17:10:18 -0300

    Create ex-1047.java

---
+++ ex-1047.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int horaInicial = entrada.nextInt();
+        int minutoInicial = entrada.nextInt();
+        int somaMinutosIniciais = horaInicial * 60 + minutoInicial;
+
+        int horaFinal = entrada.nextInt();
+        int minutoFinal = entrada.nextInt();
+        int somaMinutosFinais = horaFinal * 60 + minutoFinal;
+
+        int diferenca = (24 * 60 - somaMinutosIniciais) + somaMinutosFinais;
+
+        int horas = diferenca / 60;
+        int minutos = diferenca % 60;
+
+        if (horas >= 24 && minutos >= 1) {
+            horas = horas % 24;
+        };
+
+        System.out.printf("O JOGO DUROU %s HORA(S) E %s MINUTO(S)\n", horas, minutos);
+    }
+
+}
