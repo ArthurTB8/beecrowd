e4e3de8f9ae172c324a3ce041f1bace6fb364d98
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2022-10-03 10:59:24 -0300

    Update ex-1048.java

--- ex-1048.java
+++ ex-1048.java
@@ -1,88 +1,35 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        
-        
-        float A = entrada.nextFloat();
-
-         if ( A >= 0 || A <= 400) {
-             
-         float reajuste = A * 0.15f;
-           
-         float  novoSalario = reajuste + A;
-         
-         
-         System.out.printf("Novo salario: %.2f\n",novoSalario);
-           
-         System.out.printf("Reajuste ganho: %.2f\n",reajuste);
-             
-         System.out.printf("Em percentual: 15 %%");
-           
-           
-        } 
-        else if ( A >= 400.01 || A <= 800) {
-        
-         float reajuste = A * 0.12f;
-            
-         float  novoSalario = reajuste + A;
-         
-         
-         System.out.printf("Novo salario: %.2f\n",novoSalario);
-           
-         System.out.printf("Reajuste ganho: %.2f\n",reajuste);
-             
-         System.out.printf("Em percentual: 12 %%");
-           
+        double numero = entrada.nextDouble();
+        double r1 = numero + (numero * 0.15), m1 = (numero * 0.15);
+        double r2 = numero + (numero * 0.12), m2 = (numero * 0.12);
+        double r3 = numero + (numero * 0.10), m3 = (numero * 0.10);
+        double r4 = numero + (numero * 0.07), m4 = (numero * 0.07);
+        double r5 = numero + (numero * 0.04), m5 = (numero * 0.04);
+        if (numero >= 0.00 && numero <= 400.00) {
+            System.out.printf("Novo salario: %.2f\n", r1);
+            System.out.printf("Reajuste ganho: %.2f\n", m1);
+            System.out.println("Em percentual: 15 %");
+        } else if (numero >= 400.01 && numero <= 800.00) {
+            System.out.printf("Novo salario: %.2f\n", r2);
+            System.out.printf("Reajuste ganho: %.2f\n", m2);
+            System.out.println("Em percentual: 12 %");
+        } else if (numero >= 800.01 && numero <= 1200.00) {
+            System.out.printf("Novo salario: %.2f\n", r3);
+            System.out.printf("Reajuste ganho: %.2f\n", m3);
+            System.out.println("Em percentual: 10 %");
+        } else if (numero >= 1200.01 && numero <= 2000.00) {
+            System.out.printf("Novo salario: %.2f\n", r4);
+            System.out.printf("Reajuste ganho: %.2f\n", m4);
+            System.out.println("Em percentual: 7 %");
+        } else if (numero >= 2000.01) {
+            System.out.printf("Novo salario: %.2f\n", r5);
+            System.out.printf("Reajuste ganho: %.2f\n", m5);
+            System.out.println("Em percentual: 4 %");
         }
-         else if ( A >= 800.01 || A <= 1200) {
-            
-         float reajuste = A * 0.10f;
-           
-         float  novoSalario = (A * 0.10f) + A;
-         
-         
-         System.out.printf("Novo salario: %.2f\n",novoSalario);
-           
-         System.out.printf("Reajuste ganho: %.2f\n",reajuste);
-             
-         System.out.printf("Em percentual: 10 %%");
-           
-        }
-         else if ( A >= 1200.01 || A <= 2000) {
-           
-         float reajuste = A * 0.07f;
-           
-         float  novoSalario = reajuste + A;
-         
-         
-         System.out.printf("Novo salario: %.2f\n",novoSalario);
-           
-         System.out.printf("Reajuste ganho: %.2f\n",reajuste);
-             
-         System.out.printf("Em percentual: 7 %%");
-           
-           
-        }
-         else if (  A <= 2000.01) {
-            
-          float reajuste = A * 0.04f;
-        
-          float  novoSalario = reajuste + A;
-          
-          
-          System.out.printf("Novo salario: %.2f\n",novoSalario);
-           
-          System.out.printf("Reajuste ganho: %.2f\n",reajuste);
-             
-          System.out.printf("Em percentual: 4 %%");
-        }
-
-      
-
     }
 }
