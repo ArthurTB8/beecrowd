8fc4ea747d256e427fff25547647514d7f8750ed
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2022-10-13 17:23:49 -0300

    Update ex-1101.java

--- ex-1101.java
+++ ex-1101.java
@@ -1,54 +1,62 @@
 import java.io.IOException;
-
 import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) throws IOException {
 
- 
+        public static void main(String[] args) throws IOException {
 
-       int X, Y;
 
-        Scanner teclado =new Scanner(System.in);
 
-        
+            int X, Y;
 
-        while (((X = teclado.nextInt()) > 0 )&&((Y = teclado.nextInt()) > 0 )) {
+            Scanner teclado =new Scanner(System.in);
 
-            int sum = 0;
 
-            if (X > Y) {
 
-                for (Y = Y; Y <= X; Y++) {
+            while (true) {
 
-                    sum+=Y;
+                X = teclado.nextInt();
+                Y = teclado.nextInt();
+                int sum = 0;
 
-                    System.out.print(Y+" ");
+                if (X <= 0 || Y <= 0 ){
+                    break;
+                }
+                
+                if (X > Y) {
+
+                    for (Y = Y; Y <= X; Y++) {
+
+                        sum+=Y;
+
+                        System.out.print(Y+" ");
+
+                    }
+
+                    System.out.print("Sum="+sum+"\n");
+
+                }else {
+
+                    for (X = X; X <= Y; X++) {
+
+                        sum+=X;
+
+                        System.out.print(X+" ");
+
+                    }
+
+                    System.out.print("Sum="+sum+"\n");
 
                 }
-
-                System.out.print("Sum="+sum+"\n");
-
-            }else {
-
-                for (X = X; X <= Y; X++) {
-
-                    sum+=X;
-
-                    System.out.print(X+" ");
-
-                }
-
-                System.out.print("Sum="+sum+"\n");
 
             }
 
+
+
         }
 
- 
+
 
     }
-
- 
-
-}
+    
