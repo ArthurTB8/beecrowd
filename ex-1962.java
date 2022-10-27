3015bc776a81ed933c30f83fe10aa4d33a13a61c
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2022-10-27 10:22:06 -0300

    Update ex-1962.java

--- ex-1962.java
+++ ex-1962.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		
+		int n = entrada.nextInt();
+		
+		for(int i=0 ; i<n ; i++){
+			int t = entrada.nextInt();
+			if(t<2015)
+				System.out.println(2015-t + " D.C.");
+			else
+				System.out.println(t-2014 + " A.C.");
+		}
+	}
+}
